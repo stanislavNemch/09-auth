ba270fb2e055e998f19f9a164c65649adc2e8b4e
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-29 13:07:26 +0300

    Add client-side routing with useRouter and refresh on initial load in AuthLayout

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,7 +1,19 @@
+"use client"; // 1. Оголошуємо компонент клієнтським
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const router = useRouter();
+
+    // 2. Оновлюємо маршрутизатор при першому завантаженні
+    useEffect(() => {
+        router.refresh();
+    }, [router]);
+
     return <>{children}</>;
 }
